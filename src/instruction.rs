39d46219cbc3716139cc2fb44d335ab8c86d57ba
sustrak <josepsansprats@gmail.com>
2018-04-07 23:10:15 +0200

    init

---
+++ src/instruction.rs
@@ -0,0 +1,185 @@
+use std::collections::HashMap;
+
+#[derive(Debug,PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Instruction {
+    pub op: usize,
+    pub f : usize,
+    pub rd: usize,
+    pub ra: usize,
+    pub rb: usize,
+    pub n : usize
+}
+
+#[derive(Debug,PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Codes {
+    pub opcode: i8,
+    pub f: i8
+}
+
+pub struct InsDic {
+    pub m_code_loc: HashMap<String, Instruction>,
+    pub parse_ins: HashMap<String, String>,
+    pub op_codes: HashMap<String, Codes>,
+    pub uses: HashMap<String, Vec<String>>
+
+}
+
+
+pub fn init_ins_dic() -> InsDic {
+    let mut ins_dic = InsDic {
+        m_code_loc: HashMap::new(),
+        parse_ins: HashMap::new(),
+        op_codes: HashMap::new(),
+        uses: HashMap::new()
+    };
+    //Instruction format of Logic Arithmetic & Comp
+    let mut ins = Instruction{
+        op: 0,
+        f: 10,
+        rd: 4,
+        ra: 7,
+        rb: 13,
+        n: 64
+    };
+
+    let s: String = String::from(r"r(?P<rd>\d),\sr(?P<ra>\d),\sr(?P<rb>\d)");
+    let mut c = Codes {
+        opcode: 0,
+        f: 0
+    };
+    //Logic & Arithmetic
+    ins_dic.m_code_loc.insert(String::from("and"), ins);
+    ins_dic.m_code_loc.insert(String::from("or"), ins);
+    ins_dic.m_code_loc.insert(String::from("xor"), ins);
+    ins_dic.m_code_loc.insert(String::from("not"), ins);
+    ins_dic.m_code_loc.insert(String::from("add"), ins);
+    ins_dic.m_code_loc.insert(String::from("sub"), ins);
+    ins_dic.m_code_loc.insert(String::from("sha"), ins);
+    ins_dic.m_code_loc.insert(String::from("shl"), ins);
+
+    ins_dic.parse_ins.insert(String::from("and"), s.clone());
+    ins_dic.parse_ins.insert(String::from("or"), s.clone());
+    ins_dic.parse_ins.insert(String::from("xor"), s.clone());
+    ins_dic.parse_ins.insert(String::from("not"), s.clone());
+    ins_dic.parse_ins.insert(String::from("add"), s.clone());
+    ins_dic.parse_ins.insert(String::from("sub"), s.clone());
+    ins_dic.parse_ins.insert(String::from("sha"), s.clone());
+    ins_dic.parse_ins.insert(String::from("shl"), s.clone());
+
+    ins_dic.op_codes.insert(String::from("and"), c);
+    c.f = 1;
+    ins_dic.op_codes.insert(String::from("or"), c);
+    c.f = 2;
+    ins_dic.op_codes.insert(String::from("xor"), c);
+    c.f = 3;
+    ins_dic.op_codes.insert(String::from("not"), c);
+    c.f = 4;
+    ins_dic.op_codes.insert(String::from("add"), c);
+    c.f = 5;
+    ins_dic.op_codes.insert(String::from("sub"), c);
+    c.f = 6;
+    ins_dic.op_codes.insert(String::from("sha"), c);
+    c.f = 7;
+    ins_dic.op_codes.insert(String::from("shl"), c);
+
+
+    ins_dic.uses.insert(String::from("and"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("or"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("xor"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("not"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("add"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("sub"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("sha"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("shl"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+
+    //Comparation
+    ins_dic.m_code_loc.insert(String::from("cmplt"), ins);
+    ins_dic.m_code_loc.insert(String::from("cmple"), ins);
+    ins_dic.m_code_loc.insert(String::from("cmpeq"), ins);
+    ins_dic.m_code_loc.insert(String::from("cmpltu"), ins);
+    ins_dic.m_code_loc.insert(String::from("cmpleu"), ins);
+
+    ins_dic.parse_ins.insert(String::from("cmplt"), s.clone());
+    ins_dic.parse_ins.insert(String::from("cmple"), s.clone());
+    ins_dic.parse_ins.insert(String::from("cmpeq"), s.clone());
+    ins_dic.parse_ins.insert(String::from("cmpltu"), s.clone());
+    ins_dic.parse_ins.insert(String::from("cmpleu"), s.clone());
+
+    c.opcode = 1;
+    ins_dic.op_codes.insert(String::from("cmplt"), c);
+    c.f = 0;
+    ins_dic.op_codes.insert(String::from("cmple"), c);
+    c.f = 1;
+    ins_dic.op_codes.insert(String::from("cmpeq"), c);
+    c.f = 3;
+    ins_dic.op_codes.insert(String::from("cmpltu"), c);
+    c.f = 4;
+    ins_dic.op_codes.insert(String::from("cmpleu"), c);
+    c.f = 5;
+
+    ins_dic.uses.insert(String::from("cmplt"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("cmple"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("cmpeq"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("cmpltu"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+    ins_dic.uses.insert(String::from("cmpleu"), vec![String::from("rd"), String::from("ra"), String::from("rb")]);
+
+    //ADDI
+    ins.f = 64; ins.rb = 64;
+    ins.n = 10;
+    c.opcode = 2; c.f = 0;
+    ins_dic.m_code_loc.insert(String::from("addi"), ins);
+    ins_dic.parse_ins.insert(String::from("addi"), String::from(r"r(?P<rd>\d),\sr(?P<ra>\d),\s(?P<n>\d{1,})"));
+    ins_dic.op_codes.insert(String::from("addi"), c);
+    ins_dic.uses.insert(String::from("addi"), vec![String::from("rd"), String::from("ra"), String::from("n")]);
+
+    //LOAD
+    c.opcode = 3; c.f = 0;
+    ins_dic.m_code_loc.insert(String::from("ld"), ins);
+    ins_dic.parse_ins.insert(String::from("ld"), String::from(r"r(?P<rd>\d),\s(?P<n>-?\d{1,})\(r(?P<ra>\d)\)"));
+    ins_dic.op_codes.insert(String::from("ld"), c);
+    ins_dic.uses.insert(String::from("ld"), vec![String::from("rd"), String::from("ra"), String::from("n")]);
+
+    //STORE
+    c.opcode = 4; c.f = 0;
+    ins_dic.m_code_loc.insert(String::from("st"), ins);
+    ins_dic.parse_ins.insert(String::from("st"), String::from(r"(?P<n>-?\d{1,})\(r(?P<ra>\d)\),\sr(?P<rd>\d)"));
+    ins_dic.op_codes.insert(String::from("st"), c);
+    ins_dic.uses.insert(String::from("st"), vec![String::from("rd"), String::from("ra"), String::from("n")]);
+
+    //LDB
+    c.opcode = 13;
+    ins_dic.m_code_loc.insert(String::from("ldb"), ins);
+    ins_dic.parse_ins.insert(String::from("ldb"), String::from(r"r(?P<rd>\d),\s(?P<n>-?\d{1,})\(r(?P<ra>\d)\)"));
+    ins_dic.op_codes.insert(String::from("ldb"), c);
+    ins_dic.uses.insert(String::from("ldb"), vec![String::from("rd"), String::from("ra"), String::from("n")]);
+
+    //STB
+    c.opcode = 14;
+    ins_dic.m_code_loc.insert(String::from("stb"), ins);
+    ins_dic.parse_ins.insert(String::from("stb"), String::from(r"(?P<n>-?\d{1,})\(r(?P<ra>\d)\),\sr(?P<rd>\d)"));
+    ins_dic.op_codes.insert(String::from("stb"), c);
+    ins_dic.uses.insert(String::from("stb"), vec![String::from("rd"), String::from("ra"), String::from("n")]);
+
+    //MOV
+    c.opcode = 5; c.f = 0;
+    ins.f = 7;
+    ins.ra = 64;
+    ins.n = 8;
+    ins_dic.m_code_loc.insert(String::from("movi"), ins);
+    ins_dic.m_code_loc.insert(String::from("movhi"), ins);
+
+    ins_dic.parse_ins.insert(String::from("movi"), String::from(r"r(?P<rd>\d),\s(?P<n>\d{1,})"));
+    ins_dic.parse_ins.insert(String::from("movhi"), String::from(r"r(?P<rd>\d),\s(?P<n>\d{1,})"));
+
+    ins_dic.op_codes.insert(String::from("movi"), c);
+    c.f = 1;
+    ins_dic.op_codes.insert(String::from("movhi"), c);
+
+    ins_dic.uses.insert(String::from("movi"), vec![String::from("rd"), String::from("n")]);
+    ins_dic.uses.insert(String::from("movhi"), vec![String::from("rd"), String::from("n")]);
+
+    
+
+    ins_dic
+}
+
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+extern crate regex;
+
+mod instruction;
+
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use regex::Regex;
+use std::collections::HashMap;
+
+
+use instruction::{InsDic, Instruction, init_ins_dic};
+
+fn create_hex(pos: &Instruction, s: &mut String, h: &HashMap<&str, i8>) {
+    //opcode
+    s.insert_str(pos.op, &format!("{:01$b}", h.get("op").unwrap(), 4));
+    if pos.rb != 64 {
+        s.insert_str(pos.rd, &format!("{:01$b}", h.get("rd").unwrap(), 3));
+        s.insert_str(pos.ra, &format!("{:01$b}", h.get("ra").unwrap(), 3));
+        s.insert_str(pos.f, &format!("{:01$b}", h.get("f").unwrap(), 3));
+        s.insert_str(pos.rb, &format!("{:01$b}", h.get("rb").unwrap(), 3));
+    }
+    else if pos.ra == 64 {
+        s.insert_str(pos.rd, &format!("{:01$b}", h.get("rd").unwrap(), 3));
+        s.insert_str(pos.f, &format!("{:01$b}", h.get("f").unwrap(), 1));
+        let aux = format!("{:01$b}", h.get("n").unwrap(), 8);
+        s.insert_str(pos.n, &aux[aux.len()-8..aux.len()]);
+    }
+    else {
+        s.insert_str(pos.rd, &format!("{:01$b}", h.get("rd").unwrap(), 3));
+        s.insert_str(pos.ra, &format!("{:01$b}", h.get("ra").unwrap(), 3));
+        let aux = format!("{:01$b}", h.get("n").unwrap(), 6);
+        s.insert_str(pos.n, &aux[aux.len()-6..aux.len()]);
+    }
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file_name = &args[1];
+
+    let mut f = File::open(file_name).expect("File not found");
+    let mut out_f = File::create(file_name.replace(".s", ".hex")).unwrap();
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents).expect("Couldn't read form the file");
+
+    let dict: InsDic = init_ins_dic();
+
+    let r_ins_name = Regex::new(r"[a-zA-Z]{2,}").expect("Couldn't create the r_ins_name re expresion");
+
+    for l in contents.lines() {
+        let mut ins0 = r_ins_name.captures(&l).unwrap();
+        let ins_name = &ins0[0];
+        
+        let mut r_ins_parse: Regex;
+        
+        match dict.parse_ins.get(ins_name) {
+            Some(re) => r_ins_parse = Regex::new(&re).expect("Couldn't create the r_ins_parse re expresion"),
+            None => panic!("{} is not a instruction.", ins_name)
+        }
+
+        let mut ins_b = HashMap::new();
+        
+        ins_b.insert("op", dict.op_codes.get(ins_name).unwrap().opcode);
+        ins_b.insert("f", dict.op_codes.get(ins_name).unwrap().f);
+        let c = r_ins_parse.captures(&l).unwrap();
+        for item in dict.uses.get(ins_name).unwrap() {
+            if item == "n" && (ins_name == "st" || ins_name == "ld") {
+                ins_b.insert(item, c[item.as_str()].to_string().parse::<i8>().unwrap()/2);
+            }
+            else {
+                ins_b.insert(item, c[item.as_str()].to_string().parse::<i8>().unwrap());
+            }
+
+        }
+        let mut out_s: String = String::with_capacity(16);
+
+        let ins_positions = dict.m_code_loc.get(ins_name).unwrap();
+
+        create_hex(&ins_positions, &mut out_s, &ins_b);
+        
+        writeln!(out_f, "{:01$x}", i32::from_str_radix(&out_s, 2).unwrap(), 4);
+    }
+}
